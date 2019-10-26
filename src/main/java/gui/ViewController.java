f3693d30f9ad7cecdeedf8a49beb8019581e35d2
INGRID <ingridcarolinelima@hotmail.com>
2019-10-26 19:12:11 -0300

    adicionado package gui

---
+++ src/main/java/gui/ViewController.java
@@ -0,0 +1,33 @@
+package gui;
+
+import org.jboss.jandex.Main;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class ViewController {
+	
+	@FXML
+	private TextField usuario;
+	@FXML
+	private TextField senha;
+	
+	@FXML
+	public void onClickButtonLogin() {
+		
+		try {
+			Stage menuSystem = new Stage();
+			Parent parent = FXMLLoader.load(getClass().getResource("/gui/menuSystemView.fxml"));
+			Scene scene = new Scene(parent);
+			menuSystem.setScene(scene);
+			menuSystem.show();
+	
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}

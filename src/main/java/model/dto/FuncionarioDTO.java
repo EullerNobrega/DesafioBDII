ba6220ff302ca7760d70bf2d044933711c74e1b8
Euller <euller@incorptech.com.br>
2019-11-20 12:13:17 -0300

    Update ControllerCombustivelCliente.java and FuncionarioDTO.java

---
+++ src/main/java/model/dto/FuncionarioDTO.java
@@ -0,0 +1,35 @@
+package model.dto;
+
+public class FuncionarioDTO {
+	private String nomeFuncionario;
+	private String cpf;
+	private String cnpj;
+	private double valorTotal;
+	
+	
+	public String getNomeFuncionario() {
+		return nomeFuncionario;
+	}
+	public void setNomeFuncionario(String nomeFuncionario) {
+		this.nomeFuncionario = nomeFuncionario;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public double getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+}
